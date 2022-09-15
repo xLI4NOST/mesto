8f4151055a933a29446fce61ab7c80970f6af970
LI4NOST <xli4nost@mail.ru>
2022-09-15 12:01:09 +0300

    Создать кнопки и попап

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,19 @@ const menu = document.querySelector('.menu');
 const saveButton = menu.querySelector('.menu__button');
 const closeButton = menu.querySelector('.menu__close-icon');
 const popup = page.querySelector('.popup');
-const like = document.querySelectorAll('.card__button-like');
+const like = document.querySelector('.card__button-like');
 const textName = document.querySelector('.form__input_type_name');
 const textSubtitle = document.querySelector('.form__input_type_job');
 const profileNameText = document.querySelector('.profile__name');
 const profileSubtitleText = document.querySelector('.profile__subtitle');
 const formElement = popup.querySelector('.form');
+const popupCards = document.querySelector('.popup-cards');
+const addButton = document.querySelector(".profile__add-button");
+const closeCardsButton = document.querySelector ('.menu__card-close');
+const cardTemplate = document.querySelector ('#card-template').content
+const container = document.querySelector ('.elements')
+
+console.log(cardTemplate);
 
 function openEditProfilePopup() {
     popup.classList.add('popup_active');
@@ -18,8 +25,16 @@ function openEditProfilePopup() {
     textSubtitle.value = profileSubtitleText.textContent;
 }
 
+function openEditCardsPopup() {
+    popupCards.classList.add ('popup-cards_active');
+}
+
 function closeEditProfilePopup() {
     popup.classList.remove('popup_active');
+}
+
+function closeEditCardsPopup(){
+    popupCards.classList.remove ('popup-cards_active');
 }
 
 function formSubmitHandler(evt) {
@@ -29,14 +44,19 @@ function formSubmitHandler(evt) {
     closeEditProfilePopup();
 }
 
-// function likeActive() {
-//     like.forEach( item => { item.classList.toggle('card__button-like_active')})
-// }
 
-// like.forEach( item => { item.addEventListener('click', likeActive ) } )
-
-
+closeCardsButton.addEventListener ('click', closeEditCardsPopup)
 closeButton.addEventListener('click', closeEditProfilePopup);
 editButton.addEventListener('click', openEditProfilePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+addButton.addEventListener ('click', openEditCardsPopup)
+
+// like.addEventListener ('click', function (evt){
+//     evt.target.classList.toggle ('card__button-like_active')
+// })
+
+
+
+// songElement.querySelector('.song__like').addEventListener('click', function (evt) {
+//     evt.target.classList.toggle ('song__like_active')
 
