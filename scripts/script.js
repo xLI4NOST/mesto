a35faeb7fb903e5f72c4937ae67edb2f20947335
LI4NOST <xli4nost@mail.ru>
2022-09-17 12:53:39 +0300

    Добавить элементы массива в контейнер с помощью js

--- scripts/script.js
+++ scripts/script.js
@@ -14,22 +14,56 @@ const formElement = popup.querySelector('.form');
 const popupCards = document.querySelector('.popup-cards');
 const addButton = document.querySelector(".profile__add-button");
 const closeCardsButton = document.querySelector('.menu__card-close');
-const imageCard = document.querySelector ('.form__input_type_link')
-const titleCard = document.querySelector ('form__input_type_text')
-// const cardTemplate = document.querySelector('#card-template').content;
-const container = document.querySelector('.elements');
-const cardCreate = document.querySelector('.card_create');
-// const cardTemplate = document.querySelector ('#card-template').content;
-// const cardCreate = document.querySelector ('.card_create');
-
-function addCard() {
-    const template = document.querySelector('#card-template')
-    const cardTemplate = document.querySelector('#card-template').content
-    const cardElement = cardTemplate.querySelector ('.card').cloneNode(true)
-    cardElement.querySelector ('.card__image').content = imageCard.value
-    cardElement.querySelector ('.card__title').textContent = titleCard.value
-    container.append ('cardElement');
+const cardDelet = document.querySelector ('.card__button-delete')
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+  const container = document.querySelector ('.elements')
+  const cardTemplate = document.querySelector ('.card-template').content
+  const formButton = document.querySelector ('.card_create')
+  function render() {
+	initialCards.forEach(renderItem);
+    
 }
+// функция рендеренга
+function renderItem (cardData){
+    const newCard = cardTemplate.cloneNode(true) //Клоним кароточку
+    const cardTitle = newCard.querySelector ('.card__title')
+    const cardImage = newCard.querySelector ('.card__image')
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name
+    cardTitle.textContent = cardData.name;
+    container.append(newCard);
+}
+render()
+
+function cardDelete (event) {
+
+}
+
 
 function openEditProfilePopup() {
     popup.classList.add('popup_active');
@@ -55,8 +89,7 @@ function formSubmitHandler(evt) {
     profileSubtitleText.textContent = textSubtitle.value;
     closeEditProfilePopup();
 }
-
-cardCreate.addEventListener('click', addCard)
+// cardCreate.addEventListener ('click', addCard)
 closeCardsButton.addEventListener('click', closeEditCardsPopup)
 closeButton.addEventListener('click', closeEditProfilePopup);
 editButton.addEventListener('click', openEditProfilePopup);
