bf059767999b7dc134fbfea786f3a368bee9bdb2
LI4NOST <xli4nost@mail.ru>
2022-10-27 15:21:21 +0300

    Сделать интерфейс работы с карточками

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,76 @@
 class Card {
-    constructor (title, image, ){
+    constructor(title, image) {
         this._cardTitle = title
         this._cardImage = image
     }
-    _getTemplate (){
-        const cardTemplate = document.querySelector ('.card-template')
-        .content
-        .querySelector ('.card')
-        .cloneNode(true)
+    _getTemplate() {
+        const cardTemplate = document.querySelector('.card-template')
+            .content
+            .querySelector('.card')
+            .cloneNode(true)
         return cardTemplate
     }
 
-    generateCard(){
+    generateCard() {
         const cardTitle = document.querySelector('.card__title')
         const cardImage = document.querySelector('.card__image')
         this._element = this._getTemplate()
-        this._element.querySelector ('.card__image').src = this._cardImage
-        this._element.querySelector ('.card__image').alt = this._cardTitle
-        this._element.querySelector ('.card__title').textContent = this._cardTitle
+        this._element.querySelector('.card__image').src = this._cardImage
+        this._element.querySelector('.card__image').alt = this._cardTitle
+        this._element.querySelector('.card__title').textContent = this._cardTitle
         this._setEventListiners();
         return this._element
     }
-    _handleOpenPopup(){
+    _handleOpenPopup() {
         popupImages.src = this._cardImage
         popupImages.alt = this._cardTitle
         openPopup(popupImg)
     }
-    // _hendleDeleteCard (event){
-    //     const cardDeleteButton = document.querySelector('.card__button-delete');
-    //     this._currentCard = event.target.closest ('.card')
-    //     this._currentCard.remove()
-    // }
-    _handleAddCard (){
-        const card = new Card ()
-        const item = card.generateCard()
-        item.title = mestoName.value
-        item.src = mestoLink.value
-        container.prepend(item)
+    _handleDeleteCard (event){
+        this._currentCard = event.target.closest ('.card')
+        this._currentCard.remove()
     }
-    _setEventListiners (){
-        const img = this._element.querySelector ('.card__image')
-        img.addEventListener ('click', ()=>{
+    _handleLikeCard (evt){
+        this._currentLike = evt.target.closest ('.card__button-like')
+        this._currentLike.classList.toggle('card__button-like_active')
+    }
+ 
+    _setEventListiners() {
+        const img = this._element.querySelector('.card__image')
+        img.addEventListener('click', () => {
             this._handleOpenPopup()
         })
-        saveButton.addEventListener ('click', (evt)=>{
-            evt.preventDefault()
-            this._handleAddCard()
+        this._element.querySelector ('.card__button-delete')
+        .addEventListener ('click', (event)=>{
+            this._handleDeleteCard(event)
         })
+        this._element.querySelector ('.card__button-like')
+        .addEventListener ('click', (event)=>{
+            this._handleLikeCard(event)
+        })
+        
     }
 }
+//Добавление новой карточки
+function handleAddCard() {
+    const card = new Card(mestoName.value, mestoLink.value)
+    const item = card.generateCard()
+    container.prepend(item)
+}
+//Обработчик
+saveButton.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    handleAddCard()
+    closePopup(popupCards)
+})
 
-initialCards.forEach ((item) => {
-    const card = new Card (item.name, item.link)
+//Добавление карточек с массива
+initialCards.forEach((item) => {
+    const card = new Card(item.name, item.link)
     const cardElement = card.generateCard()
-    container.prepend (cardElement)
-}
-)
+    container.prepend(cardElement)
+})
 
-
-
-// Создать обработчики для кнопок
-function setListenersForButtons(element) {
-    // const cardDeleteButton = element.querySelector('.card__button-delete');
-    // cardDeleteButton.addEventListener('click', handleDeleteCard);
-    const cardLikeButton = element.querySelector('.card__button-like');
-    cardLikeButton.addEventListener('click', handleLikeCard);
-    const cardScreen = element.querySelector('.card__image');
-    cardScreen.addEventListener('click', handleOpenCardImagePreview);
-}
 
 
 // //Удаление карточки
@@ -101,8 +104,8 @@ function openPopup(popupElem) {
     document.addEventListener('keydown', closeEsc)
 };
 
-function closeEsc (e){
-    if (e.keyCode === esc ){
+function closeEsc(e) {
+    if (e.keyCode === esc) {
         const popupElem = document.querySelector('.popup_active');
         closePopup(popupElem);
     }
@@ -119,7 +122,7 @@ function closePopup(popupElem) {
 //Закрытие popup по пустому месту
 allPopUps.forEach((popupElem) => {
     popupElem.addEventListener('click', (evt) => {
-        if(evt.currentTarget === evt.target) {
+        if (evt.currentTarget === evt.target) {
             closePopup(popupElem);
         }
     })
@@ -144,7 +147,7 @@ addPopupEventHandlers(popupEditProfile, (evt) => {
 });
 
 function fillInFormInputs() {
-    textName.value = profileNameText.textContent 
+    textName.value = profileNameText.textContent
     textSubtitle.value = profileSubtitleText.textContent
 }
 
